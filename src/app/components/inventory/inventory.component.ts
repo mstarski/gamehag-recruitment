5876d51b31eb7e16ad431d06d4d0e39c911f0c5c
Michał Starski <michalstarski99@gmail.com>
2019-11-09 21:57:20 +0100

    Checkpoint - InventoryComponent retrieves group names

--- src/app/components/inventory/inventory.component.ts
+++ src/app/components/inventory/inventory.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { InventoryService } from "../../services/inventory.service";
+import { GroupName } from "../../typedefs/GroupName";
 
 @Component({
-  selector: 'app-inventory',
-  templateUrl: './inventory.component.html',
-  styleUrls: ['./inventory.component.scss']
+  selector: "app-inventory",
+  templateUrl: "./inventory.component.html",
+  styleUrls: ["./inventory.component.scss"]
 })
 export class InventoryComponent implements OnInit {
+  constructor(private inventoryService: InventoryService) {}
 
-  constructor() { }
+  groups: GroupName[] = [];
 
-  ngOnInit() {
+  getGroups(): void {
+    this.inventoryService
+      .getGroupNames()
+      .subscribe(groupNames => (this.groups = groupNames));
   }
 
+  ngOnInit() {
+    this.getGroups();
+  }
 }
