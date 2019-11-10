d4e075b670b66c014d4bec37005da5cca005f8c7
Michał Starski <michalstarski99@gmail.com>
2019-11-10 14:53:09 +0100

    Checkpoint - layout almost ready

--- src/app/components/inventory-slot/inventory-slot.component.ts
+++ src/app/components/inventory-slot/inventory-slot.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Slot } from "../../typedefs/Slot";
+import { ActionService } from "../../services/action.service";
+import { Item } from "../../typedefs/Item";
 
 @Component({
   selector: "app-inventory-slot",
@@ -7,10 +9,14 @@ import { Slot } from "../../typedefs/Slot";
   styleUrls: ["./inventory-slot.component.scss"]
 })
 export class InventorySlotComponent implements OnInit {
-  constructor() {}
+  constructor(private actionService: ActionService) {}
 
   @Input()
   props: Slot;
+
+  selectSlot(_: MouseEvent, data: Item): void {
+    this.actionService.selectItem(data);
+  }
 
   ngOnInit() {}
 }
--- src/app/components/preview/preview.component.ts
+++ src/app/components/preview/preview.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActionService } from "../../services/action.service";
+import { Item } from "../../typedefs/Item";
 
 @Component({
-  selector: 'app-preview',
-  templateUrl: './preview.component.html',
-  styleUrls: ['./preview.component.scss']
+  selector: "app-preview",
+  templateUrl: "./preview.component.html",
+  styleUrls: ["./preview.component.scss"]
 })
 export class PreviewComponent implements OnInit {
+  constructor(private actionService: ActionService) {}
 
-  constructor() { }
+  selectedItem: Item;
 
   ngOnInit() {
+    this.actionService.selectedItem.subscribe(
+      data => (this.selectedItem = data)
+    );
   }
-
 }
